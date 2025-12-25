6504493816d0ebf3d825e5395a5f7d328c4c8f2c
ANKUSH202638 <ankush@sysprotech.com>
2025-12-25 19:58:17 +0530

    fix the train issue

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -76,7 +76,7 @@ xgb_model = xgb.XGBClassifier(scale_pos_weight=class_weight, random_state=42)
 
 # Define hyperparameter grid
 param_grid = {
-    'xgbclassifier__n_estimators': 100,          # balanced bias–variance
+    'xgbclassifier__n_estimators': [100],          # balanced bias–variance
     'xgbclassifier__max_depth': 3,               # prevents overfitting
     'xgbclassifier__colsample_bytree': 0.5,      # good feature subsampling
     'xgbclassifier__colsample_bylevel': 0.5,     # stabilizes tree splits
